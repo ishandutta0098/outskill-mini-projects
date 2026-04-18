aadd855374049f46658540ba46a211c2e0461c62
Ishan <ishandutta0098@gmail.com>
2026-04-19 00:44:58 +0530

    Add intermediate/06 dep_vuln_scanner tools

---
+++ projects/intermediate/06_dep_vuln_scanner/tools.py
@@ -0,0 +1,11 @@
+import os
+
+from crewai_tools import EXASearchTool, FileReadTool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+deps_reader_tool = FileReadTool()
+
+os.environ["EXA_API_KEY"] = os.getenv("EXA_API_KEY")
+exa_search_tool = EXASearchTool()
